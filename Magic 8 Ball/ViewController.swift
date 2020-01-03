f43af5212cc2d11c228475b1bf0f9c199867480e
Mohammad Agung <diditkazama@gmail.com>
2020-01-03 14:36:06 +0700

    update ke swift 5

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,28 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+//    swift 4
+//    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+//    swift 5
+    let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+    
     var randomBall: Int = 0
     
     @IBOutlet weak var magicBall: UIImageView!
-    
-    func updateBall() {
-        randomBall = Int.random(in: 0 ... 4)
-        
-        magicBall.image = UIImage(named: ballArray[randomBall])
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateBall()
     }
+    
+    func updateBall() {
+        randomBall = Int.random(in: 0 ... 4)
 
-    @IBAction func askButton(_ sender: Any) {
-        updateBall()
+//        swift 4
+//        magicBall.image = UIImage(named: ballArray[randomBall])
+        
+//        swift 5
+        magicBall.image = ballArray[randomBall]
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateBall()
+    }
+
+    @IBAction func askButton(_ sender: Any) {
         updateBall()
     }
 }
